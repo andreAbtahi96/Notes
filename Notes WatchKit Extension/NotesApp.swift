4d81be86768c32db4feb5bf5bb031a9cf2b7c821
andreAbtahi96 <sabtahi@uci.edu>
2021-08-15 22:32:38 -0700

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Andre Abtahi on 8/15/21.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
